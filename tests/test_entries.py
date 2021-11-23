34c3082e1a5b634eeee2e9b94f6c2c7a12e80076
Adam Foldvari <adam.foldvari@emarsys.com>
2021-11-23 18:21:43 +0100

    Refactor Entry tests

--- tests/test_entries.py
+++ tests/test_entries.py
@@ -5,13 +5,16 @@ from healthcheck_api.entries import EntryModel
 import unittest
 
 
-class TestEntryModule(unittest.TestCase):
+class TestEntry(unittest.TestCase):
 
     def setUp(self):
         self.app = create_app({'TESTING': True})
         self.client = self.app.test_client()
         with self.app.app_context():
             db.create_all()
+            new_entry = EntryModel('test_entry')
+            db.session.add(new_entry)
+            db.session.commit()
 
     def tearDown(self):
         with self.app.app_context():
@@ -19,11 +22,6 @@ class TestEntryModule(unittest.TestCase):
             db.drop_all()
 
     def test_get_entries(self):
-        with self.app.app_context():
-            new_entry = EntryModel('test_entry')
-            db.session.add(new_entry)
-            db.session.commit()
-
         response = self.client.get('/entries')
         assert response.json == [{"id": 1, "name": 'test_entry'}]
 
@@ -31,33 +29,18 @@ class TestEntryModule(unittest.TestCase):
         response = self.client.post('/entries',
                                     data=json.dumps({"name": "test_entry"}),
                                     content_type='application/json')
-        assert response.json == {"id": 1, "name": 'test_entry'}
+        assert response.json == {"id": 2, "name": 'test_entry'}
 
     def test_get_entry(self):
-        with self.app.app_context():
-            new_entry = EntryModel('test_entry')
-            db.session.add(new_entry)
-            db.session.commit()
-
         response = self.client.get('/entries/1')
         assert response.json == {"id": 1, "name": 'test_entry'}
 
     def test_update_entry(self):
-        with self.app.app_context():
-            new_entry = EntryModel('test_entry')
-            db.session.add(new_entry)
-            db.session.commit()
-
         response = self.client.put('/entries/1',
                                    data=json.dumps({"name": "new_name"}),
                                    content_type='application/json')
         assert response.json == {"id": 1, "name": 'new_name'}
 
     def test_delete_entry(self):
-        with self.app.app_context():
-            new_entry = EntryModel('test_entry')
-            db.session.add(new_entry)
-            db.session.commit()
-
         response = self.client.delete('/entries/1')
         assert response.status_code == 204
